0816c08346d5402dec62d83dd8b70dc4371a532f
xVanTuring <xVanTuring@foxmail.com>
2023-07-28 16:19:57 +0800

    basic lsp configuration

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,46 +1,63 @@
 -- local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
 
 return require("packer").startup(
-    function()
-	use "wbthomason/packer.nvim"
-	use "Mofiqul/vscode.nvim"
-	use "nvim-tree/nvim-tree.lua"
-	use "nvim-tree/nvim-web-devicons"
-	use "nvim-lualine/lualine.nvim"
-	use "lukas-reineke/indent-blankline.nvim"
+function()
+    use "wbthomason/packer.nvim"
+    use "Mofiqul/vscode.nvim"
+    use "nvim-tree/nvim-tree.lua"
+    use "nvim-tree/nvim-web-devicons"
+    use "nvim-lualine/lualine.nvim"
+    use "lukas-reineke/indent-blankline.nvim"
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
-    use {
-        'glepnir/dashboard-nvim',
-        event = 'VimEnter',
-        config = function()
-            require('dashboard').setup {
-                -- project = { enable = true, limit = 8, },
-                -- config
-            }
-        end,
-        requires = {'nvim-tree/nvim-web-devicons'}
-    }
     use "APZelos/blamer.nvim"
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.2',
-        -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-    use 'neovim/nvim-lspconfig'
---    use 'simrat39/rust-tools.nvim'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
 
---    use {'neoclide/coc.nvim', branch = 'release'}
     use 'lewis6991/gitsigns.nvim'
     use 'romgrk/barbar.nvim'
-    -- use 'airblade/vim-gitgutter'
-    end
+    use({
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        requires = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim",
+        },
+        config = function() 
+            require("telescope").load_extension("lazygit")
+        end,
+    })
+    use {
+        'goolord/alpha-nvim',
+        requires = { 'nvim-tree/nvim-web-devicons' },
+        config = function ()
+            require'alpha'.setup(require'alpha.themes.startify'.config)
+        end
+    }
+    use {
+        'jedrzejboczar/possession.nvim',
+        requires = { 'nvim-lua/plenary.nvim' },
+    }
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+    use {
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+    }
+    use({
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!:).
+        run = "make install_jsregexp"
+    })
+end
 )
 
